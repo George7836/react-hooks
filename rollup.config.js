4cedc8714dbcba4733ca86479dd4cab3383f14ed
George Aliev <gera08012329@gmail.com>
2023-07-11 08:31:36 +0300

    fix: change package.json and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,28 @@
 import typescript from "rollup-plugin-typescript2"
-import postcss from "rollup-plugin-postcss"
-import pkg from "./package.json"
+
+const pkg = require('./package.json')
+
 export default {
   input: "src/index.ts",
   output: [
     {
       file: pkg.main,
       format: "cjs",
-      exports: "named",
       sourcemap: true,
       strict: false,
     },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
   ],
   plugins: [
-    postcss({
-      extract: false,
-    }),
-    typescript({ objectHashIgnoreUnknownHack: true }),
+    typescript({ 
+      rollupCommonJSResolveHack: false,
+      clean: true,
+     }),
   ],
   external: ["react", "react-dom"],
 }
